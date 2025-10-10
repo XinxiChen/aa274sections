972b6a8f01ee78b22062b4b2d53b08c24bc5e8ad
Laszlo Bollyky <lbollyky@stanford.edu>
2025-10-09 19:11:06 -0700

    done

--- s2_basic/scripts/constant_control.py
+++ s2_basic/scripts/constant_control.py
@@ -2,7 +2,7 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import String, Bool
 from geometry_msgs.msg import Twist
 
 
@@ -12,9 +12,12 @@ class TimerNode(Node):
 
         # self.timer_pub = self.create_publisher(String, "/timer", 10)
         self.twist_pub = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.kill_sub = self.create_subscription(Bool, "/kill", self.kill_callback, 10)
+
 
         self.timer = self.create_timer(0.2, self.timer_callback)
 
+        self.killed = False
 
         self.counter = 0
 
@@ -28,11 +31,27 @@ class TimerNode(Node):
         twist_msg.linear.x = 0.5  # Constant forward velocity
         twist_msg.angular.z = 0.0  # No rotation
 
+        if self.killed:
+            twist_msg.linear.x=0.0
+            twist_msg.linear.y=0.0
+            twist_msg.linear.z=0.0
+            twist_msg.angular.x=0.0
+            twist_msg.angular.y=0.0
+            twist_msg.angular.z=0.0
+
+
         self.twist_pub.publish(twist_msg)
         # self.timer_pub.publish(timer_msg)
 
         self.counter += 1
         # self.get_logger().info(f"Published message {self.counter}: {timer_msg.data}")
+
+        if self.killed:
+            self.timer.cancel()
+
+
+    def kill_callback(self, msg: Bool):
+        self.killed = msg.data
 
 
 if __name__ == "__main__":
